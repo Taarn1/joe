f001df2c90281e4c90d37d6d32ab387a831e9c6d
IdaDavidsen <118819940+IdaDavidsen@users.noreply.github.com>
2023-10-30 11:11:51 +0100

    client mappe
    meget af koden er fra øvelse 4

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 //express boilerplate code
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 //import routes
-const testRoutes = require('./routes/example');
+const testRoutes = require("./routes/example");
 //use routes on /test
-app.use('/test', testRoutes);
-
+app.use("/test", testRoutes);
 
 //forside
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`Joe app listening on port ${port}!`));
