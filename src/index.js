85ab93ec05f1d4a8b0c77f9af66f8b9fd15f09f4
IdaDavidsen <118819940+IdaDavidsen@users.noreply.github.com>
2023-10-30 11:39:26 +0100

    routes som ikke helt virker
    med kodestykker fra Ø4

--- src/index.js
+++ src/index.js
@@ -8,10 +8,25 @@ const fetch = (...args) =>
 
 //import routes
 const testRoutes = require("./routes/example");
+const userRoute = require("./routes/users.route");
 //use routes on /test
 app.use("/test", testRoutes);
 
+app.use("/", userRoute);
+
 //forside
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/pages/home.html"));
+});
+
+app.get("/home.js", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/scripts/home.js"));
+});
+
+app.get("/global.css", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/styles/global.css"));
+});
 
 app.listen(port, () => console.log(`Joe app listening on port ${port}!`));
--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,8 +1,102 @@
 const express = require("express");
+const path = require("path");
+const userRoute = express.Router();
+
+/* fra 2. semester
 const router = express.Router();
 const usersController = require("../controllers/users.controller");
+*/
+
+let id = 1;
+let db = [];
 
 // endpoints
+userRoute.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../client/pages/login.html"));
+});
+
+userRoute.get("/login.js", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../client/scripts/login.js"));
+});
+userRoute.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../client/pages/register.html"));
+});
+
+userRoute.get("/register.js", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../client/scripts/register.js"));
+});
+
+userRoute.get("/global.css", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../client/styles/global.css"));
+});
+
+userRoute
+  .get("/user", (req, res) => {
+    res.send(db);
+  })
+  .post("/user", (req, res) => {
+    let data = req.body;
+    let user = {};
+    user = data;
+    user.id = id;
+    id++;
+    db.push(user);
+    res.send("User added");
+  })
+  .get("/user/:id", (req, res) => {
+    let response = "";
+    let i = 0;
+    while (i < db.length) {
+      if (req.params.id == db[i].id) {
+        response = db[i];
+        break;
+      }
+      i++;
+      if (i == db.length) {
+        response = "User not found";
+      }
+    }
+    res.send(response);
+  })
+  .delete("/user/:id", (req, res) => {
+    let response = "";
+    let i = 0;
+    while (i < db.length) {
+      if (req.params.id == db[i].id) {
+        response = `User with ID ${db[i].id} deleted`;
+        db.splice(i, 1);
+        // res.send(db[i])
+        break;
+      }
+      i++;
+      if (i == db.length) {
+        response = "User not found";
+      }
+    }
+    res.send(response);
+  })
+  .post("/login", (req, res) => {
+    //Log in here
+    let login = req.body;
+    let response = "";
+    let i = 0;
+    while (i < db.length) {
+      if (
+        login.username == db[i].username &&
+        login.password == db[i].password
+      ) {
+        response = "Login successful";
+        break;
+      }
+      i++;
+      if (i == db.length) {
+        response = "Login failed";
+      }
+    }
+    res.send(response);
+  });
+
+/* fra 2. semester 
 router.post("/login", (req, res) => usersController.login(req, res));
 
 router.get("/logout", (req, res) => usersController.logout(req, res));
@@ -15,19 +109,6 @@ router.put("/update/:id", (req, res) => usersController.updateUser(req, res));
 router.delete("/delete/:id", (req, res) =>
   usersController.deleteUser(req, res)
 );
+*/
 
-router.post("/categories", (req, res) =>
-  usersController.favoriteCategories(req, res)
-);
-
-router.post("/read", (req, res) => usersController.readArticles(req, res));
-
-router.post("/favoriteArticles", (req, res) =>
-  usersController.favoriteArticles(req, res)
-);
-
-router.get("/getFavoriteArticles", (req, res) =>
-  usersController.getFavoriteArticles(req, res)
-);
-
-module.exports = router;
+module.exports = userRoute;
