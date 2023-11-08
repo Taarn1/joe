c6700635eee6d8f3466c3f90e4f1d671e6784d82
Avisonix <131752864+Avisonix@users.noreply.github.com>
2023-11-08 13:03:51 +0100

    Database udvidet

--- lavDatabase.js
+++ lavDatabase.js
@@ -7,19 +7,42 @@ const db = new sqlite.Database('./joeDatabase.sqlite', (err) => {
     }
     console.log('Connected to the lav database.');
 });
+// Alt det følgende er bare for at lave tabellerne i databasen.
+/*sql = `
+    CREATE TABLE IF NOT EXISTS pictures (
+        picid INTEGER PRIMARY KEY AUTOINCREMENT,
+        picurl TEXT NOT NULL
+    );
+`; 
+sql = `
+    CREATE TABLE IF NOT EXISTS users (
+        userid INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL,
+        email TEXT NOT NULL,
+        password TEXT NOT NULL,
+        age INTEGER NOT NULL,
+        number INTEGER NOT NULL, 
+        prefered_cafe TEXT NOT NULL,
+        picid INTEGER,
+        FOREIGN KEY (picid) REFERENCES pictures(picid)
+        
+    );
+`;
+sql = `
+    CREATE TABLE IF NOT EXISTS menu (
+    itemid INTEGER PRIMARY KEY AUTOINCREMENT,
+    itemname TEXT NOT NULL
+    );
+`; 
 
-/* sql = `CREATE TABLE IF NOT EXISTS pictures ( 
-    picid INTEGER PRIMARY KEY AUTOINCREMENT, 
-    picurl TEXT NOT NULL
-);`; 
+sql = `
+    CREATE TABLE IF NOT EXISTS orders (
+        orderid INTEGER PRIMARY KEY AUTOINCREMENT,
+        userid INTEGER,
+        itemid INTEGER,
+        FOREIGN KEY (userid) REFERENCES users(userid),
+        FOREIGN KEY (itemid) REFERENCES menu(itemid)
+    );
+`; 
 db.run(sql);
-
-sql= `CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
-    email TEXT NOT NULL,
-    password TEXT NOT NULL,
-    picid INTEGER,
-    FOREIGN KEY (picid) REFERENCES pictures(picid)
-);`; 
-db.run(sql); */
+*/
