07f176fc0d6ea94b4e25a9d78fd03f0c635a1c7f
Taarn1 <112751515+Taarn1@users.noreply.github.com>
2023-11-08 09:59:25 +0100

    fix?

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -11,6 +11,7 @@ router.post("/signup", (req, res) => usersController.signUp(req, res));
 router.get("/getUser/:id", (req, res) => usersController.getUser(req, res));
 
 router.put("/update/:id", (req, res) => usersController.updateUser(req, res));
+
 router.delete("/delete/:id", (req, res) =>
   usersController.deleteUser(req, res)
 );
