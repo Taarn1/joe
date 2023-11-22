f488125645bbaa768fcc5c4a291dd9523370fd3a
IdaDavidsen <118819940+IdaDavidsen@users.noreply.github.com>
2023-11-22 09:41:46 +0100

    små ændringer og slettede ting som fav artikler

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,19 +4,16 @@ const router = express.Router();
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
 
-
 router.post("/login", (req, res) => usersController.login(req, res));
 
 router.get("/logout", (req, res) => usersController.logout(req, res));
 
-router.post("/signup", jsonParser, (req, res) => usersController.signUp(req, res));
+router.post("/signup", jsonParser, (req, res) =>
+  usersController.signUp(req, res)
+);
 
 router.get("/getUser/:id", (req, res) => usersController.getUser(req, res));
 
 router.put("/update/:id", (req, res) => usersController.updateUser(req, res));
-
-router.delete("/delete/:id", (req, res) =>
-  usersController.deleteUser(req, res)
-);
 
 module.exports = router;
