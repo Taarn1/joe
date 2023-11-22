bea3bce08e92e8b968e13f327d04a217a1cdd978
SofieJegind <131752845+SofieJegind@users.noreply.github.com>
2023-11-22 20:27:08 +0100

    Socket server connection
    Har brugt express dokumentation eller hvad man kalder det, men er i tvivl om der skal bruges HTTPS

--- src/index.js
+++ src/index.js
@@ -8,6 +8,23 @@ const app = express();
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
+// socket.io server er i tvivl om jeg skal bruge express eller https
+const {createServer} = require("http");
+const {Server} = require("socket.io");
+const httpServer = createServer(app);
+const io = new Server(httpServer, {})
+
+io.on("connection", (socket) => {
+  // taget fra øvelse 3
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+  socket.on("user joined", (username) => {
+    console.log(username + " joined the chat");
+    io.emit("chat message", username + " joined the chat");
+  });
+});
+  
 //import routes
 const userRoutes = require("./routes/users.routes");
 const pageRoutes = require("./routes/page.routes");
@@ -15,6 +32,7 @@ const pageRoutes = require("./routes/page.routes");
 app.use("/user", userRoutes);
 app.use("/", pageRoutes);
 
+<<<<<<< Updated upstream
 app.get("/global.css", (req, res) =>
   res.sendFile(path.join(__dirname, "../client/styles/global.css"))
 );
@@ -23,3 +41,6 @@ app.get("/chat.css", (req, res) =>
 );
 
 app.listen(port, () => console.log(`Joe app listening on port ${port}!`));
+=======
+httpServer.listen(port, () => console.log(`Joe app listening on port ${port}!`));
+>>>>>>> Stashed changes
