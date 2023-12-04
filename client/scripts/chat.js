8b9324c457e4a289d1a1de74ac05e83b995a37ed
Taarn1 <112751515+Taarn1@users.noreply.github.com>
2023-12-04 11:11:26 +0100

    chat fuldt ud

--- client/scripts/chat.js
+++ client/scripts/chat.js
@@ -9,10 +9,8 @@ const input = document.getElementById("input");
 // bruger 
 
 //getcookie
-console.log(document.cookie);
 const userIdCookie = document.cookie.split(";")
 const userId = userIdCookie.find((cookie) => cookie.includes("userId")).split("=")[1];
-console.log(userId);
 
 //send to login if no cookie
 //ikke lavet endnu
@@ -20,15 +18,23 @@ console.log(userId);
 
 //ikke lavet endnu
 const clearChatWindow = () => {
+    sendButton.onclick = () => {};
     while(seeMessages.firstChild){
         seeMessages.removeChild(seeMessages.firstChild);
     }
 }
 
-const send = (input) => {
-    chatroom.sendMessage(input.message);
-    input.value = "";
-}
+sendButton.addEventListener("click", () => {
+    const messageToSend = input.value; // Få indholdet fra inputfeltet
+    if (activeChatroom) {
+      const chatroom = new Chatroom(activeChatroom);
+      chatroom.sendMessage(messageToSend);
+      input.value = ''; // Nulstil inputfeltet efter at beskeden er sendt
+    } else {
+      console.error('Ingen aktivt chatrum valgt.');
+      // Håndter fejlen her, hvis der ikke er valgt et aktivt chatrum
+    }
+  });
 
 //skal bruges til at lytte til det aktive chatroom
 let activeChatroom = null;
@@ -42,28 +48,18 @@ const match = fetch(`http://localhost:3000/user/getmatches/${userId}`)
         result.forEach(match => {
             const matcheduser = match.user1 !== userId ? match.user2 : match.user1; // kig på dette
             const chatbutton = document.createElement("button");
-            chatbutton.addEventListener("click", () => {
-                clearChatWindow(seeMessages)
-                console.log("match_id" + match.match_id);
-                let chatroom = new Chatroom(match.match_id);
-                
-                activeChatroom = chatroom.roomname; //tror ikke helt det virker endnu
-                sendButton.addEventListener("click", () => { //virker ikke helt. Eventlisteneren bliver ved med at blive tilføjet så hvis du klikker på chat ->
-                    //2 gange, sender du beskeden 2 gange
-                    console.log(sendButton); //ccheck 
-                    //tror man kan fjerne eventlisteneren med removeEventListener i en funktion
-
-                    send(input);
-                });
-
+            let chatroom = new Chatroom(match.match_id);
                 //det er her, man lytter på beskeder
-                socket.on(chatroom.roomname, (message) => {
-                    if(chatroom.active){
-                    const messageElement = document.createElement("li");
-                    messageElement.innerHTML = message;
-                    seeMessages.appendChild(messageElement);
-                }});
-            
+            socket.on(chatroom.roomname, (message) => {
+                if(activeChatroom === chatroom.roomname){
+                const messageElement = document.createElement("li");
+                messageElement.innerHTML = message;
+                seeMessages.appendChild(messageElement);
+            }});
+            chatbutton.addEventListener("click", () => {
+                clearChatWindow()
+                console.log("match_id" + match.match_id);
+                activeChatroom = chatroom.roomname;                             
                 
             });
             chatbutton.innerHTML = "chat med " + matcheduser; // kig på dette
@@ -81,7 +77,6 @@ class Chatroom {
     sendMessage(message) {
         //måske kan vi remove eventlisteneren her
         socket.emit(this.roomname, message);
-        console.log("user sent: " + message + "to room: " + this.roomname);
     }
 }
 
